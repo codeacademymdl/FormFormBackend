e4ea69d32dc4e7459adf4380b6aff6e5a33fb7a6
vibhore.vg <vibhore97@gmail.com>
2019-02-27 10:48:19 +0530

    hapi server with healthcheck

---
+++ server.js
@@ -0,0 +1,19 @@
+const Hapi = require('hapi');
+const routes = require('./src/routes')
+
+
+const server = new Hapi.Server({
+  port: 8080,
+  host: '0.0.0.0',
+});
+
+
+
+server.route(routes);
+if (!module.parent) {
+  server.start();
+}
+console.log('Server running at port 8080');
+
+
+module.exports = server;
