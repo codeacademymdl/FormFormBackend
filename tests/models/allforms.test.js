7ce946adde0e1997ac88f94e837f5f385dda1d26
vibhore.vg <vibhore97@gmail.com>
2019-03-06 09:43:10 +0530

    added 'allforms' model's tests

--- tests/models/allforms.test.js
+++ tests/models/allforms.test.js
@@ -5,11 +5,17 @@ describe('generate', () => {
     await Model.allforms.truncate();
   });
   it('should insert forms details in the table', async () => {
-    await Model.allforms.generate(1, 'feedback', 'How you doin?', { name: 'email', email: 'email' });
+    await Model.allforms.generate(1, 'feedback', 'How you doin?', { name: 'name', email: 'email' });
     expect(await Model.allforms.count()).toEqual(1);
   });
   it('should return zero when no value is inserted', async () => {
     expect(await Model.allforms.count()).toEqual(0);
+  });
+
+  it('should return all data from table', async () => {
+    await Model.allforms.generate(2, 'feedbacks', 'How you doin?', { name: 'names', email: 'email', phone: 'phone' });
+    const formData = await Model.allforms.getAllContent();
+    expect(formData.length > 0).toBe(true);
   });
 });
 afterAll(() => {
